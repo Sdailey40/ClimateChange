c6755cad9d9373a57d847e14d9a05afe1bec31b1
Dion Davis <dion.r.davis@gmail.com>
2018-09-29 00:02:40 -0400

    Dion's last update

--- SeaLevelData/Static/logic.js
+++ SeaLevelData/Static/logic.js
@@ -1,41 +1,41 @@
 // Function to determine marker size based on population
 
-#function markerSize(population) {
- # return population;
-#}
+//function markerSize(population) {
+ // return population;
+//}
 
 // An array containing all of the information needed to create city and state markers
-#var locations = 
+//var locations = 
 
 
 // Define arrays to hold created city and state markers
-#var cityMarkers = [];
-#var stateMarkers = [];
+//var cityMarkers = [];
+//var stateMarkers = [];
 
 // Loop through locations and create city and state markers
-#for (var i = 0; i < locations.length; i++) {
+//for (var i = 0; i < locations.length; i++) {
   // Setting the marker radius for the state by passing population into the markerSize function
-  stateMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "white",
-      radius: markerSize(locations[i].state.population)
-    })
-  );
+  //stateMarkers.push(
+  //  L.circle(locations[i].coordinates, {
+  //    stroke: false,
+  //    fillOpacity: 0.75,
+  //    color: "white",
+  //    fillColor: "white",
+  //    radius: markerSize(locations[i].state.population)
+  //  })
+  //);
 
   // Setting the marker radius for the city by passing population into the markerSize function
-  cityMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "purple",
-      fillColor: "purple",
-      radius: markerSize(locations[i].city.population)
-    })
-  );
-#}
+  //cityMarkers.push(
+  //  L.circle(locations[i].coordinates, {
+  //    stroke: false,
+  //    fillOpacity: 0.75,
+  //    color: "purple",
+  //    fillColor: "purple",
+  //    radius: markerSize(locations[i].city.population)
+  //  })
+  //);
+//}
 function getPins() {
   bounds = maps.getBounds();
   url = "mongodb://localhost:27017/Dion_db" + bounds.get().latitude + "longitude" + bounds.get().longitude
@@ -54,6 +54,27 @@ function pinTheMap(data) {
         markerLayerGroup = L.LayerGroup(markerArray).addTO(map);
 
   
+
+// Create two separate layer groups: one for cities and one for states
+//var states = L.layerGroup(stateMarkers);
+//var cities = L.layerGroup(cityMarkers);
+
+
+map.on('dragend', getPins);
+map.on('zoomend', getPins);
+map.whenReady(getPins)
+// Create a baseMaps object
+//var baseMaps = {
+//  "Street Map": streetmap,
+//  "Dark Map": darkmap
+//};
+
+// Create an overlay object
+//var overlayMaps = {
+ // "State Population": states,
+  //"City Population": cities
+};
+
 // Define variables for our base layers
 var streetmap = L.tileLayer({
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -69,35 +90,16 @@ var darkmap = L.tileLayer({
   accessToken: API_KEY
 });
 
-// Create two separate layer groups: one for cities and one for states
-#var states = L.layerGroup(stateMarkers);
-#var cities = L.layerGroup(cityMarkers);
-
-
-map.on('dragend', getPins);
-map.on('zoomend', getPins);
-map.whenReady(getPins)
-// Create a baseMaps object
-var baseMaps = {
-  "Street Map": streetmap,
-  "Dark Map": darkmap
-};
-
-// Create an overlay object
-#var overlayMaps = {
- # "State Population": states,
-  #"City Population": cities
-};
-
 // Define a map object
 var myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5,
-  layers: [streetmap, states, cities]
+  //layers: [streetmap, states, cities]
+  layers: [streetmap]
 });
 
 // Pass our map layers into our layer control
 // Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+//L.control.layers(baseMaps, overlayMaps, {
+//  collapsed: false
+//}).addTo(myMap);
